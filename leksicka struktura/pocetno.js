b4377301140cfe48cacf63f51c44d33980b78e39
natasa288 <120023089+natasa288@users.noreply.github.com>
2023-02-14 18:23:28 +0100

    Add files via upload

---
+++ leksicka struktura/pocetno.js
@@ -0,0 +1,24 @@
+var  fristName, lastName, price, discount, fullPrice;
+
+fristName = "John";
+lastName = "Doe";
+price = 19.90;
+discount = 0.10;
+
+var x = 3;
+x = 1;
+if (x == 1){
+    x = 2;
+}
+ 
+var a = true;
+  if (a == true) {
+      var z = 44;
+      return 0;
+  }
+
+  if (discount != 0){
+fullPrice = (price * 100) / discount;
+  } else {
+      fullPrice = price;
+  }
